4e5492d2a0b49a008d81aee57ba3717be0eb5506
mperea2unm <32257922+mperea2unm@users.noreply.github.com>
2017-09-25 01:23:47 -0600

    Add files via upload

---
+++ Basic_Robot_Control.ino
@@ -0,0 +1,54 @@
+#include <Servo.h>
+Servo Motor_Left;
+Servo Motor_Right;
+const int MF = 20;//motor forward
+const int MB = 138;//motor backward
+const int MS = 91;//motor stop
+
+
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_SHIELD
+
+#include <OneSheeld.h>
+
+void setup() 
+{
+  OneSheeld.begin();
+
+  //motor pin config
+  Motor_Left.attach(11);
+  Motor_Right.attach(12);
+
+}
+
+void loop()
+{
+  if(GamePad.isUpPressed())//foward
+  {
+    Motor_Left.write(MF);
+    Motor_Right.write(MF);
+  } 
+  else if(GamePad.isDownPressed())//backward
+  {
+    Motor_Left.write(MB);
+    Motor_Right.write(MB);
+  }
+
+  else if(GamePad.isRightPressed())//CW
+  {
+    Motor_Left.write(MF);
+    Motor_Right.write(MB);
+  }
+
+  else if(GamePad.isLeftPressed())//CCW
+  {
+    Motor_Left.write(MB);
+    Motor_Right.write(MF);
+  }
+
+  else
+  {
+    Motor_Left.write(MS);
+    Motor_Right.write(MS);
+  }
+}
